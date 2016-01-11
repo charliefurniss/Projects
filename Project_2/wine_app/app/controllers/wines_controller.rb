dce21785773443310725d798cbfb109fe09272dd
Charlie Furniss <charliefurniss@MacBook-Pro-4.local>
2016-01-11 14:55:06 +0000

    created wines and users controllers

---
+++ Project_2/wine_app/app/controllers/wines_controller.rb
@@ -0,0 +1,22 @@
+class WinesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
